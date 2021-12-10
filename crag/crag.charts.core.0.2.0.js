33153842e4f5db75abe7dc4b9fecacd66ead0b9b
Craig Hancock <65733827+c-hancock@users.noreply.github.com>
2021-12-10 17:34:28 +0000

    Update crag.charts.core.0.2.0.js
    Made color pallet a class

--- crag/crag.charts.core.0.2.0.js
+++ crag/crag.charts.core.0.2.0.js
@@ -1,76 +1,253 @@
-// Hello.
-//
-// This is JSHint, a tool that helps to detect errors and potential
-// problems in your JavaScript code.
-//
-// To start, simply enter some JavaScript anywhere on this page. Your
-// report will appear on the right side.
-//
-// Additionally, you can toggle specific options in the Configure
-// menu.
+class ColorPallet {
 
-const pallet = {
-	red: '#f44336',
-	pink: '#ec407a',
-	purple: '#ba68c8',
-	deeppurple: '#7e57c2',
-	indigo: '#5c6bc0',
-	blue: '#2196f3',
-	lightblue: '#03a9f4',
-	cyan: '#00bcd4',
-	teal: '#009688',
-	green: '#4caf50',
-	lightgreen: '#8bc34a',
-	lime: '#cddc39',
-	yellow: '#ffeb3b',
-	amber: '#ffc107',
-	orange: '#ff9800',
-	deeporange: '#ff5722',
-	brown: '#8d6e63',
-	grey: '#9e9e9e',
-	bluegrey: '#607d8b',
-	darkbluegrey: '#192841',
-	black: '#000000',
-	almostblack: '#222222',
-	charcoal: '#333333',
-	darkgrey: '#555555',
-	lightgrey: '#DDDDDD',
-	white: '#FFFFFF',
-	multi: null,
-	match: null,
-	positiveNegative: null,
-	key: function(n) {
-		return this[Object.keys(this)[n]];
+	pallet = {
+		red: '#f44336',
+		pink: '#ec407a',
+		purple: '#ba68c8',
+		deeppurple: '#7e57c2',
+		indigo: '#5c6bc0',
+		blue: '#2196f3',
+		lightblue: '#03a9f4',
+		cyan: '#00bcd4',
+		teal: '#009688',
+		green: '#4caf50',
+		lightgreen: '#8bc34a',
+		lime: '#cddc39',
+		yellow: '#ffeb3b',
+		amber: '#ffc107',
+		orange: '#ff9800',
+		deeporange: '#ff5722',
+		brown: '#8d6e63',
+		grey: '#9e9e9e',
+		bluegrey: '#607d8b',
+		darkbluegrey: '#192841',
+		black: '#000000',
+		almostblack: '#222222',
+		charcoal: '#333333',
+		darkgrey: '#555555',
+		lightgrey: '#DDDDDD',
+		white: '#FFFFFF',
 	}
-};
 
-function isPalletColor(col) {
-	return pallet.hasOwnProperty(col);
+	modes = ['match', 'multi', 'redGreen']
+
+	/**
+	 * Resolves a color value, from either a hex code, pallet name or mode.
+	 * @param {string} value Pallet id, hex code or color mode.
+	 * @return {string} Returns hex code. Or returns mode name if valid mode provided
+	 */
+	resolveColor(value) {
+
+		/**
+		 * Check to see if value is one of the acceptable modes
+		 */
+		if (value in this.modes) return value;
+
+		/**
+		 * Check to see if value is name of color from pallet
+		 */
+		if (this.pallet.hasOwnProperty(value)) return this.pallet[value];
+
+		/**
+		 * Check to see if the value is a valid hex code color.
+		 */
+		if (this._isValidHexColor(value)) return value;
+
+		/**
+		 * Default to white
+		 */
+		return '#FFFFFF';
+
+	}
+
+	/**
+	 * Resolves a color value, from either a hex code, pallet name or mode.
+	 * @param {string} value Pallet id, hex code or color mode.
+	 * @return {boolean} True if value is pallet name, hex code or mode
+	 */
+	isValidColor(value) {
+
+		/**
+		 * Check to see if value is one of the acceptable modes
+		 */
+		if (value in this.modes) return true;
+
+		/**
+		 * Check to see if value is name of color from pallet
+		 */
+		if (this.pallet.hasOwnProperty(value)) return true;
+
+		return this._isValidHexColor(value);
+
+	}
+
+	getColorByIndex(index) {
+
+		if (index > Object.values(this.pallet).length) {
+
+			return this.pallet.white;
+
+		}
+
+		return Object.value(this.pallet)[index];
+
+	}
+
+	getColorByMode(mode, value) {
+
+		if (value === undefined) throw "Value needs to be set for mode to function correctly";
+
+		if (mode === 'redGreen') {
+
+			if (value < 0) return this.pallet.red;
+
+			return this.pallet.green;
+
+		}
+
+		if (mode === 'multi') return Object.values(this.pallet)[value];
+
+		if (mode === 'match') return this.resolveColor(value);
+
+	}
+
+	_isValidHexColor(hexCode) {
+
+		return /^#[0-9A-F]{6}$/i.test(hexCode);
+
+	}
+
+	// _getContrastYIQ(color){
+	//
+	//  	color = this.resolveColor(color);
+	//
+	// 	 if (color === false) return '#000000';
+	//
+	// 	let r, g, b;
+	//
+	// 	if (color.includes('rgb')) {
+	//
+	// 		color = color.replace('rgb(', '').replace(')', '');
+	//
+	// 		r = color.split(',')[0].trim();
+	// 		g = color.split(',')[1].trim();
+	// 		b = color.split(',')[2].trim();
+	//
+	// 	} else {
+	//
+	// 		color = color.replace("#", "");
+	//
+	// 		r = parseInt(color.substr(0,2),16);
+	// 		g = parseInt(color.substr(2,2),16);
+	// 		b = parseInt(color.substr(4,2),16);
+	//
+	// 	}
+	//
+	// 	let yiq = ((r*299)+(g*587)+(b*114))/1000;
+	//
+	// 	return (yiq >= 128) ? 'darkgrey' : 'white';
+	//
+	// }
+
+	getContrastColor(color) {
+
+		color = this.resolveColor(color);
+
+		if (color === false) return '#000000';
+
+		let r, g, b;
+
+		if (color.includes('rgb')) {
+
+			color = color.replace('rgb(', '').replace(')', '');
+
+			r = color.split(',')[0].trim();
+			g = color.split(',')[1].trim();
+			b = color.split(',')[2].trim();
+
+		} else {
+
+			color = color.replace("#", "");
+
+			r = parseInt(color.substr(0,2),16);
+			g = parseInt(color.substr(2,2),16);
+			b = parseInt(color.substr(4,2),16);
+
+		}
+
+		let yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000;
+
+		return (yiq >= 140) ? this.pallet.charcoal : this.pallet.white;
+
+	}
+
 }
+//
+// /**
+//  * @typedef {pallet} Built in available color pallet
+//  */
+// const pallet = {
+// 	red: '#f44336',
+// 	pink: '#ec407a',
+// 	purple: '#ba68c8',
+// 	deeppurple: '#7e57c2',
+// 	indigo: '#5c6bc0',
+// 	blue: '#2196f3',
+// 	lightblue: '#03a9f4',
+// 	cyan: '#00bcd4',
+// 	teal: '#009688',
+// 	green: '#4caf50',
+// 	lightgreen: '#8bc34a',
+// 	lime: '#cddc39',
+// 	yellow: '#ffeb3b',
+// 	amber: '#ffc107',
+// 	orange: '#ff9800',
+// 	deeporange: '#ff5722',
+// 	brown: '#8d6e63',
+// 	grey: '#9e9e9e',
+// 	bluegrey: '#607d8b',
+// 	darkbluegrey: '#192841',
+// 	black: '#000000',
+// 	almostblack: '#222222',
+// 	charcoal: '#333333',
+// 	darkgrey: '#555555',
+// 	lightgrey: '#DDDDDD',
+// 	white: '#FFFFFF',
+// 	multi: null,
+// 	match: null,
+// 	positiveNegative: null,
+// 	key: function(n) {
+// 		return this[Object.keys(this)[n]];
+// 	}
+// };
+//
+// function isPalletColor(col) {
+// 	return pallet.hasOwnProperty(col);
+// }
+//
+// function isValidHexColor(hexCode) {
+//
+// 	return /^#[0-9A-F]{6}$/i.test(hexCode);
+//
+// }
+//
+// function isValidColor(colorValue) {
+//
+// 	if (isPalletColor(colorValue)) return true;
+//
+// 	return isValidHexColor(colorValue);
+//
+// }
 
-function isValidHexColor(hexCode) {
-
-	return /^#[0-9A-F]{6}$/i.test(hexCode);
-
-}
-
-function isValidColor(colorValue) {
-
-	if (isPalletColor(colorValue)) return true;
-
-	return isValidHexColor(colorValue);
-
-}
-
-function resolveColor(value) {
-
-	if (isPalletColor(value)) return pallet[value];
-
-	if (isValidHexColor(value)) return value;
-	
-	return '#FFFFFF';
-
-}
+// function resolveColor(value) {
+//
+// 	if (isPalletColor(value)) return pallet[value];
+//
+// 	if (isValidHexColor(value)) return value;
+//
+// 	return '#FFFFFF';
+//
+// }
 
 function formatLabel(value, type = 'number', max = 0) {
 
@@ -360,68 +537,68 @@ function ObjectLength(object) {
 	}
 	return length;
 }
-
-function getContrastYIQ(color){
-
-	if (pallet.hasOwnProperty(color)) {
-		color = pallet[color];
-	}
-
-	let r, g, b;
-
-	if (color.includes('rgb')) {
-
-		color = color.replace('rgb(', '').replace(')', '');
-
-		r = color.split(',')[0].trim();
-		g = color.split(',')[1].trim();
-		b = color.split(',')[2].trim();
-
-	} else {
-
-		color = color.replace("#", "");
-
-		r = parseInt(color.substr(0,2),16);
-		g = parseInt(color.substr(2,2),16);
-		b = parseInt(color.substr(4,2),16);
-
-	}
-
-	let yiq = ((r*299)+(g*587)+(b*114))/1000;
-
-	return (yiq >= 128) ? 'darkgrey' : 'white';
-
-}
-
-function getContrastColor(color) {
-
-	color = resolveColor(color);
-
-	let r, g, b;
-
-	if (color.includes('rgb')) {
-
-		color = color.replace('rgb(', '').replace(')', '');
-
-		r = color.split(',')[0].trim();
-		g = color.split(',')[1].trim();
-		b = color.split(',')[2].trim();
-
-	} else {
-
-		color = color.replace("#", "");
-
-		r = parseInt(color.substr(0,2),16);
-		g = parseInt(color.substr(2,2),16);
-		b = parseInt(color.substr(4,2),16);
-
-	}
-
-	let yiq = ((r*299)+(g*587)+(b*114))/1000;
-
-	return (yiq >= 140) ? pallet.charcoal : pallet.white;
-
-}
+//
+// function getContrastYIQ(color){
+//
+// 	if (pallet.hasOwnProperty(color)) {
+// 		color = pallet[color];
+// 	}
+//
+// 	let r, g, b;
+//
+// 	if (color.includes('rgb')) {
+//
+// 		color = color.replace('rgb(', '').replace(')', '');
+//
+// 		r = color.split(',')[0].trim();
+// 		g = color.split(',')[1].trim();
+// 		b = color.split(',')[2].trim();
+//
+// 	} else {
+//
+// 		color = color.replace("#", "");
+//
+// 		r = parseInt(color.substr(0,2),16);
+// 		g = parseInt(color.substr(2,2),16);
+// 		b = parseInt(color.substr(4,2),16);
+//
+// 	}
+//
+// 	let yiq = ((r*299)+(g*587)+(b*114))/1000;
+//
+// 	return (yiq >= 128) ? 'darkgrey' : 'white';
+//
+// }
+//
+// function getContrastColor(color) {
+//
+// 	color = resolveColor(color);
+//
+// 	let r, g, b;
+//
+// 	if (color.includes('rgb')) {
+//
+// 		color = color.replace('rgb(', '').replace(')', '');
+//
+// 		r = color.split(',')[0].trim();
+// 		g = color.split(',')[1].trim();
+// 		b = color.split(',')[2].trim();
+//
+// 	} else {
+//
+// 		color = color.replace("#", "");
+//
+// 		r = parseInt(color.substr(0,2),16);
+// 		g = parseInt(color.substr(2,2),16);
+// 		b = parseInt(color.substr(4,2),16);
+//
+// 	}
+//
+// 	let yiq = ((r*299)+(g*587)+(b*114))/1000;
+//
+// 	return (yiq >= 140) ? pallet.charcoal : pallet.white;
+//
+// }
 Element.prototype.insertChildAtIndex = function(child, index) {
   if (!index) index = 0
   if (index >= this.children.length) {
