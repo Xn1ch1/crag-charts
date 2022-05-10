0f9a04ea8d0701abc02e72e5c2da8530e5ee427b
Craig Hancock <65733827+c-hancock@users.noreply.github.com>
2022-05-10 12:21:08 +0100

    Fixed issue where target wasn't properly reset

--- crag.progress.1.1.1.js
+++ crag.progress.1.1.1.js
@@ -401,6 +401,11 @@ class CragProgress extends CragCore {
 			this.chart.lines.target.style.left = '0px';
 			this._lineSmall(this.chart.lines.target);
 			this._hideTarget();
+			
+			this.chart.labels.target.style.left = `4px`;
+			this.chart.valueLabels.target.style.left = `4px`;
+
+			this.chart.valueLabels.target.textContent = `0`;
 
 		} else {
 
