50e0a10fa026ed7f668dc138fd96138c161b5270
Craig Hancock <xn1ch1@gmail.com>
2024-01-26 18:49:44 +0000

    Added exampled

---
+++ examples/src/chart-creator.1.0.0.js
@@ -0,0 +1,11 @@
+function showMenu() {
+
+	document.getElementById('menu-modal').style.display = '';
+
+}
+
+function hideMenu() {
+	
+	document.getElementById('menu-modal').style.display = 'none';
+	
+}
