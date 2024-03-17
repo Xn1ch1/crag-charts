5e0fcfd8561a57030f8e1dcbe0017ae8b42b338b
Craig Hancock <xn1ch1@gmail.com>
2024-03-17 15:56:12 +0000

    Update crag.charts.loader.js

--- bin/crag.charts.loader.js
+++ bin/crag.charts.loader.js
@@ -20,7 +20,7 @@ class CragLoader {
         },
         combo: {
             js: ['crag.lines.1.7.0.js', 'crag.column.1.6.0.js', 'crag.combo.1.7.0.js'],
-            css: 'crag.combo.1.0.2.css'
+            css: ['crag.lines.1.0.3.css', 'crag.column.1.0.2.css', 'crag.combo.1.0.2.css']
         },
         pie: {
             js: 'crag.pie.1.4.0.js',
