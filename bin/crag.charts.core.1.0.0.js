2af4fe5f529af2b702adeef609a58e6bbd68a9cd
Craig Hancock <65733827+c-hancock@users.noreply.github.com>
2024-05-09 13:17:05 +0100

    Update crag.charts.core.1.0.0.js
    Removed depreciated subsrt

--- bin/crag.charts.core.1.0.0.js
+++ bin/crag.charts.core.1.0.0.js
@@ -387,9 +387,9 @@ class CragCore {
 
             color = color.replace("#", "");
 
-            r = parseInt(color.substr(0,2),16);
-            g = parseInt(color.substr(2,2),16);
-            b = parseInt(color.substr(4,2),16);
+            r = parseInt(color.substring(0,2),16);
+            g = parseInt(color.substring(2,4),16);
+            b = parseInt(color.substring(4,6),16);
 
         }
 
